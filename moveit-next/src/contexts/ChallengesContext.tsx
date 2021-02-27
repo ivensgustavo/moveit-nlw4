111ca5f5d3820ee87359a3f49f590742630aae9d
ivensgustavo <gustavo_ivens@alu.ufc.br>
2021-02-26 23:18:24 -0300

    Implement business rules

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,11 +1,21 @@
 import { createContext, ReactNode, useState } from 'react';
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 
 interface ChallengesContextData {
   level: number;
   currenteExperience: number;
+  experienceToNextLevel: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -19,21 +29,35 @@ export function ChallengesContextProvider({ children }: ChallengesProviderProps)
   const [currenteExperience, setCurrenteExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('New challenge');
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
   }
 
   return (
     <ChallengesContext.Provider value={{
       level,
       currenteExperience,
+      experienceToNextLevel,
       challengesCompleted,
+      activeChallenge,
       levelUp,
-      startNewChallenge
+      startNewChallenge,
+      resetChallenge
     }}>
       {children}
     </ChallengesContext.Provider>
